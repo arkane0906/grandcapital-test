fc691f36a826eefed882772bc04af3b0139ae8ae
Eugen Idelbaev <arkane0906@gmail.com>
2016-12-29 00:04:44 +0300

    add Menu model

--- menu/models.py
+++ menu/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
+class Menu(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=100)
+
+    def root_items(self):
+        return MenuItem.objects.filter(menu=self, level=1)
+
+
 class MenuItem(models.Model):
     name = models.CharField(verbose_name='Название', max_length=100)
-    url = models.CharField(verbose_name='Адрес ссылки', max_length=250, blank=True, null=True)
+    url = models.CharField(verbose_name='Адрес ссылки', max_length=250)
+    menu = models.ForeignKey(Menu, verbose_name='Меню', on_delete=models.CASCADE)
     parent = models.ForeignKey('self', verbose_name='Родительский пункт меню', on_delete=models.CASCADE,
                                blank=True, null=True)
     level = models.PositiveSmallIntegerField(verbose_name='Уровень', default=1)
